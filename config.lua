4b9d118ac13646ca66b21260211593c428c5fd81
ara8586 <ara@mailfa.com>
2017-02-26 14:09:52 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = ""
+bot_token = "361263911:AAFB6YJkeHEkfFQR7jkcLTAiHYCCgctCG-A"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 212400472
+sudo_id = 71377914
